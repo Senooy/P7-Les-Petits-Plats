b335e4ca93bd09fefc009b344a2e2a8b05723e22
Dokgo <nskhelifi@gmail.com>
2023-09-08 10:15:43 +0200

    test for keywords to click listener

--- scripts/controller/recipeController.js
+++ scripts/controller/recipeController.js
@@ -9,7 +9,7 @@ export class ControllerRecipes {
 		this.tagDisplay = new FilterTagView()
 		this.keywordsDisplay = new KeywordsView()
 		this.searchInput = document.querySelector('#search-zone')
-		this.keywordsToClick = document.querySelectorAll('.accordion-body ul li')
+		// this.keywordsToClick = document.querySelectorAll('.accordion-body ul li')
 		// Je crée un tableau qui va contenir les tags sélectionnés
 		this.selectedTags = []
 		this.ingredientArray = []
@@ -51,50 +51,54 @@ export class ControllerRecipes {
 			const resetFilteredRecipes = this.filter.resetSearch(this.model.recipes)
 			if (this.mainInputLength > 3) {
 				this.view.displayRecipesList(mainFilteredRecipes)
+				this.keywordsDisplay.displayKeywordsList(mainFilteredRecipes)
 			} else if (this.mainInputLength <= 2) {
 				this.view.displayRecipesList(resetFilteredRecipes)
+				this.keywordsDisplay.displayKeywordsList(resetFilteredRecipes)
 			}
 			console.log('mainSearch du controleur :', mainFilteredRecipes)
 		})
 	}
 
-	keywordsSearch() {
-		this.keywordsToClick.forEach((keyword) => {
-			keyword.addEventListener('click', (event) => {
-				const keywordToSearch = event.target
-				// Ici, je récupère le nom de l'array dans lequel se trouve le mot clé cliqué
-				const keywordArray = keywordToSearch.closest('ul').id.replace('List', '')
-				const keywordTagToSearch = event.target.innerText
-				// Je push le mot clé cliqué dans mon tableau de tags sélectionnés
-				this.selectedTags.push(keywordTagToSearch)
-				console.log('Selected Tags :', this.selectedTags)
-				if (keywordArray === 'ingredient') {
-					// const ingredientTagFilteredRecipes = this.filter.ingredientSearch(this.model.recipes, this.selectedTags)
-					// console.log('ingredientTagFilteredRecipes :', ingredientTagFilteredRecipes);
-					this.ingredientArray.push(keywordTagToSearch)
-					// Je boucle sur mon tableau de tags sélectionnés pour les passer en paramètre à ma méthode de recherche
-					for (let i = 0; i < this.ingredientArray.length; i++) {
-						this.filter.applianceSearch(this.model.recipes, this.ingredientArray[i])
-					}
-					this.tagDisplay.add(keywordTagToSearch, keywordArray)
-					// this.view.displayRecipesList(this.model.recipes)
-				} else if (keywordArray === 'appliance') {
-					this.applianceArray.push(keywordTagToSearch)
-					// Je boucle sur mon tableau de tags sélectionnés pour les passer en paramètre à ma méthode de recherche
-					for (let i = 0; i < this.applianceArray.length; i++) {
-						this.filter.applianceSearch(this.model.recipes, this.applianceArray[i])
-					}
-					this.tagDisplay.add(keywordTagToSearch, keywordArray)
-				} else if (keywordArray === 'ustensils') {
-					this.ustensilsArray.push(keywordTagToSearch)
-					// Je boucle sur mon tableau de tags sélectionnés pour les passer en paramètre à ma méthode de recherche
-					for (let i = 0; i < this.ustensilsArray.length; i++) {
-						this.filter.ustensilsSearch(this.model.recipes, this.ustensilsArray[i])
-					}
-					this.tagDisplay.add(keywordTagToSearch, keywordArray)
-				}
-			})
-		})
+	// Méthode qui va récupérer les mots clés cliqués et les envoyer au Modèle pour filtrer les recettes et créeer les tags
+	keywordsSearch(keyword) {
+		console.log('keywordsToClick :', keyword);
+		// this.keywordsToClick.forEach((keyword) => {
+		// 	keyword.addEventListener('click', (event) => {
+		// 		const keywordToSearch = event.target
+		// 		// Ici, je récupère le nom de l'array dans lequel se trouve le mot clé cliqué
+		// 		const keywordArray = keywordToSearch.closest('ul').id.replace('List', '')
+		// 		const keywordTagToSearch = event.target.innerText
+		// 		// Je push le mot clé cliqué dans mon tableau de tags sélectionnés
+		// 		this.selectedTags.push(keywordTagToSearch)
+		// 		console.log('Selected Tags :', this.selectedTags)
+		// 		if (keywordArray === 'ingredient') {
+		// 			// const ingredientTagFilteredRecipes = this.filter.ingredientSearch(this.model.recipes, this.selectedTags)
+		// 			// console.log('ingredientTagFilteredRecipes :', ingredientTagFilteredRecipes);
+		// 			this.ingredientArray.push(keywordTagToSearch)
+		// 			// Je boucle sur mon tableau de tags sélectionnés pour les passer en paramètre à ma méthode de recherche
+		// 			for (let i = 0; i < this.ingredientArray.length; i++) {
+		// 				this.filter.applianceSearch(this.model.recipes, this.ingredientArray[i])
+		// 			}
+		// 			this.tagDisplay.add(keywordTagToSearch, keywordArray)
+		// 			// this.view.displayRecipesList(this.model.recipes)
+		// 		} else if (keywordArray === 'appliance') {
+		// 			this.applianceArray.push(keywordTagToSearch)
+		// 			// Je boucle sur mon tableau de tags sélectionnés pour les passer en paramètre à ma méthode de recherche
+		// 			for (let i = 0; i < this.applianceArray.length; i++) {
+		// 				this.filter.applianceSearch(this.model.recipes, this.applianceArray[i])
+		// 			}
+		// 			this.tagDisplay.add(keywordTagToSearch, keywordArray)
+		// 		} else if (keywordArray === 'ustensils') {
+		// 			this.ustensilsArray.push(keywordTagToSearch)
+		// 			// Je boucle sur mon tableau de tags sélectionnés pour les passer en paramètre à ma méthode de recherche
+		// 			for (let i = 0; i < this.ustensilsArray.length; i++) {
+		// 				this.filter.ustensilsSearch(this.model.recipes, this.ustensilsArray[i])
+		// 			}
+		// 			this.tagDisplay.add(keywordTagToSearch, keywordArray)
+		// 		}
+		// 	})
+		// })
 	}
 }
 
--- scripts/view/recipeView.js
+++ scripts/view/recipeView.js
@@ -7,8 +7,8 @@ export class ViewRecipes {
 		// this.controller = new ControllerRecipes()
 		// J'utilise bind pour pouvoir conserver le this de la classe ViewRecipes dans la méthode displayRecipesList
 		this.displayRecipesList = this.displayRecipesList.bind(this)
-		this.searchText = ''
-		this.mainInputLength = 0
+		// this.searchText = ''
+		// this.mainInputLength = 0
 		this.recipeSnippet = document.getElementById('recipes-zone')
 		this.searchInput = document.querySelector('#search-zone')
 	}
@@ -115,27 +115,35 @@ export class KeywordsView {
 		this.ingredientlist = []
 		this.applianceList = []
 		this.ustensilsList = []
+		this.keywordsToClick = document.querySelectorAll('.accordion-body ul li')
+
 		// this.controller.keywordsToClick = document.querySelectorAll('.accordion-body ul li')
 		this.ingredientButtonList = document.getElementById('ingredientList')
 		this.applianceButtonList = document.getElementById('applianceList')
 		this.ustensilsButtonList = document.getElementById('ustensilsList')
 	}
 
-	displayKeywordsList(recipesToShow, keywordsToClick) {
-		// const ingredientButtonList = document.getElementById('ingredientList')
-		// const applianceButtonList = document.getElementById('applianceList')
-		// const ustensilsButtonList = document.getElementById('ustensilsList')
+	displayKeywordsList(recipesToShow) {
+		// this.ingredientButtonList.innerHTML = ''
+		// this.applianceButtonList.innerHTML = ''
+		// this.ustensilsButtonList.innerHTML = ''
 
-		this.ingredientButtonList.innerHTML = ''
-		this.applianceButtonList.innerHTML = ''
-		this.ustensilsButtonList.innerHTML = ''
-
+		console.log('recipes to show :', recipesToShow)
 		recipesToShow.forEach((recipe) => {
 			// Je crée un tableau avec les ingrédients, appareils et ustensiles de chaque recette et je supprime les doublons
+
+			// Les ingrédients
+			// this.ingredientlist = []
 			recipe.ingredients.map((ingredient) => {
 				this.ingredientlist.push(`${ingredient.ingredient}`)
 			})
+
+			// Les appareils
+			// this.applianceList = []
 			this.applianceList.push(`${recipe.appliance}`)
+
+			// Les ustensiles
+			// this.ustensilsList = []
 			recipe.ustensils.map((ustensil) => {
 				this.ustensilsList.push(`${ustensil}`)
 			})
@@ -146,6 +154,7 @@ export class KeywordsView {
 			this.ustensilsList = [...new Set(this.ustensilsList)]
 
 			// Je crée les listes d'ingrédients, appareils et ustensiles correspondants aux recettes affichées
+			this.ingredientButtonList.innerHTML = ''
 			this.ingredientButtonList.innerHTML += `
 		${this.ingredientlist
 			.map((ingredient) => {
@@ -153,6 +162,7 @@ export class KeywordsView {
 			})
 			.join('')}
 		`
+			this.applianceButtonList.innerHTML = ''
 			this.applianceButtonList.innerHTML += `
 		${this.applianceList
 			.map((appliance) => {
@@ -160,6 +170,7 @@ export class KeywordsView {
 			})
 			.join('')}
 		`
+			this.ustensilsButtonList.innerHTML = ''
 			this.ustensilsButtonList.innerHTML += `
 		${this.ustensilsList
 			.map((ustensil) => {
@@ -167,6 +178,14 @@ export class KeywordsView {
 			})
 			.join('')}
 		`
+		})
+		// Je boucle sur les mots-clés pour les rendre cliquables
+		this.keywordsToClick.forEach((keyword) => {
+			keyword.addEventListener('click', (event) => {
+				const keywordToSearch = event.target
+				console.log('keyword to search :', keywordToSearch);
+				// this.controller.keywordsSearch(keywordToSearch)
+			})
 		})
 	}
 }
